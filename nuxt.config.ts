4d7644feed223d13da7e40137deb973d1af99c12
Bravco <boss.wiw@gmail.com>
2026-01-06 11:08:38 +0100

    Updated favicon, site.manifest and meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,13 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon-96x96.png", sizes: "96x96" },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        { rel: "shortcut icon", href: "/favicon.ico" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "manifest", href: "/site.webmanifest" }
+      ],
       htmlAttrs: {
         lang: "en"
       },
@@ -19,7 +26,8 @@ export default defineNuxtConfig({
       meta: [
         { name: "title", content: "PercentBet - %bet" },
         { name: "description", content: "An AI-powered tool that finds mispriced prediction markets and shows you your percentage edge." },
-        { name: "keywords", content: "prediction markets, ai, trading, prediction markets ai" }
+        { name: "keywords", content: "prediction markets, ai, trading, prediction markets ai" },
+        { name: "apple-mobile-web-app-title", content: "PercentBet" }
       ]
     }
   },
